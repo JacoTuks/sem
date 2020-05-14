7a233beb18a30bd4ebcc8ea09a80748562471633
Jaco Marais <u11024063@tuks.com>
2020-05-14 16:46:19 +0200

    WIP replicating martina

--- examples/replicate_martina.py
+++ examples/replicate_martina.py
@@ -1,3 +1,11 @@
+"""
+Attempting to replicate figure 3 from Martina's paper Confirmed traffic in LoRaWAN:pitfalls and countermeasures.
+
+I don't know the simulation duration, trying 1 day.
+Packet size? using 25 bytes as this is in the TR 45.820 report.
+
+"""
+
 import sem
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,8 +13,8 @@ import sys
 
 # Create our SEM campaign
 ns_3_dir = '../../../../jaco/Desktop/ns-3'
-script = 'confirmed-network'
-results_dir = 'cconfirmed-results'
+script = 'replicate-martina'
+results_dir = 'replicate-martina-results'
 run_new = 1
 
 if(run_new == 1):
@@ -18,13 +26,23 @@ else:
 # Parameter space
 #################
 
+#Number of end devices to include in the simulation
+nDevices_values = [100, 500, 1000 ] #, 1500, 2000]# , 2000]# , 4000 , 10000]
 
-nDevices_values = [10000] #100, 500, 1000 , 2000 , 4000 , 
-radius_values = [7500]
-simulationTime_values = [600]
-appPeriod_values = [600]
-confirmed_values = [False, True]
-runs = 1
+#The radius of the area to simulate
+radius_values = [1200]
+
+#The time for which to simulate (Seconds)
+simulationTime_values = [86400] #60 x 60 x 24 = 86400
+
+#The period in seconds to be used by periodically transmitting applications
+appPeriod_values = [30*60] # every 30 mins
+
+#Whether or not end devices require an ACK
+confirmed_values = [True]
+
+#Number of repeats (runs) to do for averaging
+runs = 10
 
 param_combinations = {
     'nDevices': nDevices_values,
@@ -145,7 +163,7 @@ for func in func_list:
     results = campaign.get_results_as_xarray(param_combinations,
                                                 func, '', runs)
 
-    #print(results)
+    print("\nResults for ", plot_legend_list[i], "\n")
     results_average = results.reduce(np.mean, 'runs')
     results_std = results.reduce(np.std, 'runs')
 
@@ -153,15 +171,26 @@ for func in func_list:
 
         avg = results_average.sel(confirmed=confirmed_flag)
         std = results_std.sel(confirmed=confirmed_flag)
-        print(std)
+
+        #iterate through for printing
+        z = 0
+        avgList = np.squeeze(avg)
+        for numNodes in nDevices_values:
+            print("For ", numNodes, " devices (conf = ", confirmed_flag, ") : ",avgList[z].values)
+            z=z+1
+        
         axs[i].errorbar(x=param_combinations['nDevices'], y=np.squeeze(avg), yerr=6*np.squeeze(std))
         axs[i].set_title(plot_legend_list[i])
         axs[i].set_ylabel("Probability")
         axs[i].set_xticks(param_combinations['nDevices'])
         axs[i].set_ylim([0,1])
        
-        if( i == 2):
-          lgd =  axs[i].legend(['Unconfirmed', 'Confirmed'], title = "Traffic type", loc=(1.01,0.5))
+        if( i == 2): #add legend to the right of second subfigure
+
+          if(len( param_combinations['confirmed']) == 2):  #test to see if we are showing both types or only confirmed
+            lgd =  axs[i].legend(['Unconfirmed', 'Confirmed'], title = "Traffic type", loc=(1.01,0.5))
+          else:
+            lgd =  axs[i].legend(['Confirmed'], title = "Traffic type", loc=(1.01,0.5))
     i=i+1
 
 
@@ -205,22 +234,10 @@ phy_noMoreTx  = np.mean(campaign.get_results_as_numpy_array(param_combinations, 
 
 
 
-print("phy_results", phy_receivedPackets)
-print("phy_interf", phy_interferedPackets)
-print("phy_gw", phy_noMoreGwPackets)
-print("phy_sen", phy_underSensitivityPackets)
-print("phy_tx", phy_noMoreTx)
-
-
-
-# X_axis_vals = np.array(param_combinations['nDevices'])
-# plt.plot(X_axis_vals, mac_succprobs)
-# plt.plot(X_axis_vals, phy_receivedPackets, '--')
-# plt.plot(X_axis_vals, phy_interferedPackets, '--.')
-# plt.plot(X_axis_vals, phy_noMoreGwPackets, '--v')
-# plt.plot(X_axis_vals, phy_underSensitivityPackets, '--o')
-# plt.plot(X_axis_vals, phy_noMoreTx, '--4')
-
-# plt.legend(["MAC success", "PHY success", "PHY interfered", "PHY lost NoMoreGw", "PHY lost sensitivity", "PHY lost GW TX"])
+# print("phy_results", phy_receivedPackets)
+# print("phy_interf", phy_interferedPackets)
+# print("phy_gw", phy_noMoreGwPackets)
+# print("phy_sen", phy_underSensitivityPackets)
+# print("phy_tx", phy_noMoreTx)
 
 plt.show()
